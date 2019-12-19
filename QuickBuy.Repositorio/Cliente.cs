44c4788f7efa53a081d01d643f2bce7725b2c8ac
LuizGustav0 <contato-luiz-gustavo@outlook.com>
2019-12-19 02:58:23 -0300

    Camada de domínio e Padrão Repository

---
+++ QuickBuy.Repositorio/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Repositorio.Repositorios;
+
+namespace QuickBuy.Repositorio
+{
+    class Cliente
+    {
+        public Cliente()
+        {
+            var usuarioRepositorio = new UsuarioRepositorio();
+            var produto = new Produto();
+            var usuario = new Usuario();
+
+            usuarioRepositorio.Adicionar(usuario);
+        }
+
+
+
+    }
+}
