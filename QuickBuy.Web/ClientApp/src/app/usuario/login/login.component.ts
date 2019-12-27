b0f6f1b848f6bf0d68ca8932fbf9879a83284bea
LuizGustav0 <contato-luiz-gustavo@outlook.com>
2019-12-27 20:03:14 -0300

    autenticação do usuário

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
 import { Usuario } from "../../modelo/usuario";
+import { Router, ActivatedRoute } from "@angular/router";
+import { UsuarioServico } from "../../servicos/usuario/usuario.servico";
 
 @Component({
   selector: "app-login",
@@ -7,18 +9,47 @@ import { Usuario } from "../../modelo/usuario";
   styleUrls: ["./login.component.css"]
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {  
   public usuario;
-   
+  public returnUrl: string;
+  public mensagem: string;
+  private ativar_spinner: boolean;
 
-  constructor() {
+
+  constructor(private router: Router, private activatedRouter: ActivatedRoute,
+              private usuarioServico: UsuarioServico) {
+
+
+  }
+
+  ngOnInit(): void {
+    this.returnUrl = this.activatedRouter.snapshot.queryParams['returnUrl'];
     this.usuario = new Usuario();
   }
 
 
   entrar() {
-    if (this.usuario.email == "luiz@email.com" && this.usuario.senha == "123") {
-      
-    }
+    this.ativar_spinner = true;
+
+    this.usuarioServico.verificarUsuario(this.usuario)
+      .subscribe(
+        usuario_json => {
+          // essa linha será executada no caso de retorno sem erros
+          this.usuarioServico.usuario = usuario_json;
+
+          if (this.returnUrl == null) {
+            this.router.navigate(['/']);
+          } else {
+            this.router.navigate([this.returnUrl]);
+          }          
+        },
+        err => {          
+          console.log(err.error);
+          this.mensagem = err.error;
+          this.ativar_spinner = false;
+        }
+      );
+
+ 
   }
 }
