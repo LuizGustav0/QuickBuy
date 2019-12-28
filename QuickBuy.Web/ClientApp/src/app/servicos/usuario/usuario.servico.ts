e127e05372bee9f033d69daa13fa778eec7861fa
LuizGustav0 <contato-luiz-gustavo@outlook.com>
2019-12-28 04:03:21 -0300

    Criado tela, componente angular e método de cadastro para usuário por API.

--- QuickBuy.Web/ClientApp/src/app/servicos/usuario/usuario.servico.ts
+++ QuickBuy.Web/ClientApp/src/app/servicos/usuario/usuario.servico.ts
@@ -35,21 +35,23 @@ export class UsuarioServico {
     this._usuario = null;
   }
 
+  get headers(): HttpHeaders {
+    return new HttpHeaders().set('content-type', 'application/json');
+  }
+
   constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     this.baseURL = baseUrl;
   }
 
   public verificarUsuario(usuario: Usuario): Observable<Usuario> {
 
-    const headers = new HttpHeaders().set('content-type', 'application/json');
-
-    var body = {
-      email: usuario.email,
-      senha: usuario.senha
-    }
-
     //this.baseURL = raiz do site que pode ser exemplo: http://www.quickbuy.com
-    return this.http.post<Usuario>(this.baseURL + "api/usuario/VerificarUsuario", body, { headers });
+    return this.http.post<Usuario>(this.baseURL + "api/usuario/VerificarUsuario", JSON.stringify(usuario), { headers: this.headers });
+  }
+
+
+  public CadastrarUsuario(usuario: Usuario): Observable<Usuario> {
+      return this.http.post<Usuario>(this.baseURL + "api/usuario", JSON.stringify(usuario), { headers: this.headers });
   }
 
 }
